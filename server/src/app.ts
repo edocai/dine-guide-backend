af86bbba652bec297889c6766b91489f8760ddba
Ali <edocaai@hotmail.com>
2023-08-28 11:39:10 +0100

    change

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import { pool } from './db';
@@ -12,11 +12,8 @@ app.use(express.json());
 
 app.use(cors());
 //GET: Get All Restaurants
-app.get('/api/v1/restaurants', async (req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+app.get('/api/v1/restaurants', async (req: Request, res: Response) => {
+
   try {
     const restaurantRatingsData = await pool.query(
       'select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id;',
@@ -35,11 +32,8 @@ app.get('/api/v1/restaurants', async (req: Request, res: Response, next: NextFun
 });
 
 //GET: Get a Restaurant
-app.get('/api/v1/restaurants/:id', async (req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+app.get('/api/v1/restaurants/:id', async (req: Request, res: Response) => {
+
   try {
     const restaurant = await pool.query(
       'SELECT * from restaurants WHERE id = $1',
@@ -62,11 +56,8 @@ app.get('/api/v1/restaurants/:id', async (req: Request, res: Response, next: Nex
 });
 
 //POST: Create a Restaurant
-app.post('/api/v1/restaurants', async (req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+app.post('/api/v1/restaurants', async (req: Request, res: Response) => {
+
   try {
     const results = await pool.query(
       'INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) returning *',
@@ -84,11 +75,8 @@ app.post('/api/v1/restaurants', async (req: Request, res: Response, next: NextFu
 });
 
 //PUT: Update Restaurants
-app.put('/api/v1/restaurants/:id', async (req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+app.put('/api/v1/restaurants/:id', async (req: Request, res: Response) => {
+
   try {
     const results = await pool.query(
       'UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 returning *',
@@ -106,11 +94,8 @@ app.put('/api/v1/restaurants/:id', async (req: Request, res: Response, next: Nex
 });
 
 //DELETE: Delete a Restaurant
-app.delete('/api/v1/restaurants/:id', async (req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+app.delete('/api/v1/restaurants/:id', async (req: Request, res: Response) => {
+
   try {
     const results = await pool.query('DELETE FROM restaurants WHERE id = $1', [
       req.params.id,
@@ -127,11 +112,8 @@ app.delete('/api/v1/restaurants/:id', async (req: Request, res: Response, next: 
 });
 
 //POST: Add a Review
-app.post('/api/v1/restaurants/:id/addReview', async (req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+app.post('/api/v1/restaurants/:id/addReview', async (req: Request, res: Response) => {
+
   try {
     const newReview = await pool.query('INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) returning *;', [req.params.id, req.body.name, req.body.review, req.body.rating]);
     res.status(201).json({
